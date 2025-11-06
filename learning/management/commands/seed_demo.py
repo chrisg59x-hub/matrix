249875989b73c5a427a9670ab285677c6dd52d5f
chrisg59x-hub <chrisg59x@gmail.com>
2025-11-06 02:46:54 +0000

    update

--- learning/management/commands/seed_demo.py
+++ learning/management/commands/seed_demo.py
@@ -37,8 +37,7 @@ class Command(BaseCommand):
             defaults={"email": "manager@matrix.local", "org": org, "biz_role": "manager", "is_staff": True},
         )
         if not manager.has_usable_password():
-            manager.set_password("manager123"); 
-            manager.save()
+            manager.set_password("manager123"), manager.save()
 
         employee, _ = User.objects.get_or_create(
             username="employee",
